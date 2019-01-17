0208d3568a8de2ae4d33656ba5ce5190bd64b11a
Joao Guilherme <joaaogui@gmail.com>
2019-01-16 23:47:40 -0500

    Chama

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" width="300" height="300">
+    <Spotify msg="Digite o nome do artista/grupo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Spotify from './components/Spotify.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Spotify
   }
 }
 </script>
