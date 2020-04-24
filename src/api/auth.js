d89ed9b0f447966360a729592196446b5060904b
João Guilherme <joaaogui@gmail.com>
2020-04-23 22:16:22 -0300

    Adds auth request

---
+++ src/api/auth.js
@@ -0,0 +1,15 @@
+import axiosInstance from "axios"
+
+const axios = axiosInstance.create({
+    baseURL: process.env.VUE_APP_AUTH_URL,
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+})
+
+let data = {
+    "grant_type": "client_credentials",
+}
+const getToken = () => axios.post('', `grant_type=client_credentials&client_id=${process.env.VUE_APP_CLIENT_ID}&client_secret=${process.env.VUE_APP_CLIENT_SECRET}`)
+
+export {
+    getToken
+}
