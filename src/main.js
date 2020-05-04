0e151653d317f3080f7a368d160c85a1c630ca12
João Guilherme <joaaogui@gmail.com>
2020-05-03 21:46:56 -0300

    Adds ga

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-100433425-3',
+  router
+});
 
 new Vue({
   router,
